d4b2c14b252328ad61300815a7d3712f2e859177
Rich Cherngchaosil <richer_po@hotmail.com>
2017-03-04 17:54:38 -0800

    ability to move piece

--- WebGUI/js/board_controller.js
+++ WebGUI/js/board_controller.js
@@ -36,7 +36,36 @@ chessApp.controller('board_controller', function($scope, chessService){
 	]
 
 	$scope.get = function(_loc){
-		console.log(_loc);
+		// console.log(_loc);
 		return chessService.getCoord(_loc);
+	}
+
+	/*** Clicking & Move ***/
+	$scope.active_click = null;
+	$scope.click = function(_loc){
+
+
+		if($scope.active_click == null){
+			// Check if there is a piece there
+			var piece = chessService.getCoord(_loc);
+
+			// Try to select a tile
+			if(piece != ""){
+				$scope.active_click = _loc;
+			}
+		} else {
+
+			// Try to move, update board
+			if($scope.active_click != _loc){
+				// The click tile is not itself(Active Tile)
+
+				// Not equal to itself, then move
+				chessService.set(_loc, chessService.getCoord($scope.active_click));
+
+				// Clear previous tile
+				chessService.set($scope.active_click, "");
+			}
+			$scope.active_click = null;
+		}
 	}
 });
--- WebGUI/js/service_chess.js
+++ WebGUI/js/service_chess.js
@@ -20,48 +20,54 @@ chessApp.service('chessService', function(){
 		return boardCoord[_location];
 	}
 
-	// Set up the board
-	set("e8", "bK");
-	set("e1", "wK");
 
-	set("d8", "bQ");
-	set("d1", "wQ");
+	var init = function(){
+		// Set up the board
+		set("e8", "bK");
+		set("e1", "wK");
 
-	set("c8", "bB");
-	set("f8", "bB");
-	set("c1", "wB");
-	set("f1", "wB");
+		set("d8", "bQ");
+		set("d1", "wQ");
 
-	set("b8", "bN");
-	set("g8", "bN");
-	set("b1", "wN");
-	set("g1", "wN");
+		set("c8", "bB");
+		set("f8", "bB");
+		set("c1", "wB");
+		set("f1", "wB");
 
-	set("a8", "bR");
-	set("h8", "bR");
-	set("a1", "wR");
-	set("h1", "wR");
+		set("b8", "bN");
+		set("g8", "bN");
+		set("b1", "wN");
+		set("g1", "wN");
 
-	set("a7", "bP");
-	set("b7", "bP");
-	set("c7", "bP");
-	set("d7", "bP");
-	set("e7", "bP");
-	set("f7", "bP");
-	set("g7", "bP");
-	set("h7", "bP");
+		set("a8", "bR");
+		set("h8", "bR");
+		set("a1", "wR");
+		set("h1", "wR");
 
-	set("a2", "wP");
-	set("b2", "wP");
-	set("c2", "wP");
-	set("d2", "wP");
-	set("e2", "wP");
-	set("f2", "wP");
-	set("g2", "wP");
-	set("h2", "wP");
+		set("a7", "bP");
+		set("b7", "bP");
+		set("c7", "bP");
+		set("d7", "bP");
+		set("e7", "bP");
+		set("f7", "bP");
+		set("g7", "bP");
+		set("h7", "bP");
+
+		set("a2", "wP");
+		set("b2", "wP");
+		set("c2", "wP");
+		set("d2", "wP");
+		set("e2", "wP");
+		set("f2", "wP");
+		set("g2", "wP");
+		set("h2", "wP");
+	}
+
+	init();
 
 	var service = {
-		getCoord : getCoord
+		getCoord : getCoord,
+		set 	 : set
 	};
 
 	return service;
